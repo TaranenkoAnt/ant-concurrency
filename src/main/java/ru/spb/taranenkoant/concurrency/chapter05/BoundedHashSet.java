e375c1d357cf983763a6dcdb0da8f5f6bac5fb61
taranenko <taranenko@reshenie-soft.ru>
2025-10-24 17:37:47 +0300

    add BoundedHashSet example

---
+++ src/main/java/ru/spb/taranenkoant/concurrency/chapter05/BoundedHashSet.java
@@ -0,0 +1,42 @@
+package ru.spb.taranenkoant.concurrency.chapter05;
+
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.Semaphore;
+
+/**
+ * {@code @author:} TaranenkoAnt
+ * {@code @createDate:} 24.10.2025
+ */
+public class BoundedHashSet<T> {
+
+    private final Set<T> set;
+    private final Semaphore sem;
+
+    public BoundedHashSet(int bound) {
+        this.set = Collections.synchronizedSet(new HashSet<>());
+        sem = new Semaphore(bound);
+    }
+
+    public boolean add(T o) throws InterruptedException {
+        sem.acquire();
+        boolean wasAdded = false;
+        try {
+            wasAdded = set.add(o);
+            return wasAdded;
+        } finally {
+            if(!wasAdded) {
+                sem.release();
+            }
+        }
+    }
+
+    public boolean remove(Object o) {
+        boolean wasRemoved = set.remove(o);
+        if (wasRemoved)
+            sem.release();
+        return wasRemoved;
+    }
+}
